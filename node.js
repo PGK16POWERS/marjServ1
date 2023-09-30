52a685ac0465b565b5ed06c9b25cb44a8d332bb4
programmer Kartier <progrmmerKartier@gmail.com>
2023-09-30 10:47:21 +0200

    server setup

---
+++ node.js
@@ -0,0 +1,15 @@
+const express = require("expres");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=> {
+    res.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+
+
+app.listen(7600, ()=> {
+    console.log("Danko Supreme");
+})
